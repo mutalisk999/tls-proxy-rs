c9a26dd67bc8f06f6d87541fd80e4c56289b7cbc
mutalisk999 <tangjian999999999@gmail.com>
2022-11-15 00:38:13 +0800

    update main.rs

--- tls-proxy-server/src/main.rs
+++ tls-proxy-server/src/main.rs
@@ -5,13 +5,119 @@ use std::net::SocketAddr;
 use std::error::Error;
 use tokio::net::{TcpSocket, TcpStream};
 use config::{load_entire_file_content, ServerConfig, CONFIG_FILE_NAME};
-use tokio_tls_helper::{ServerTlsConfig, Certificate, Identity};
-use log::{error, info};
+use tokio_tls_helper::{ServerTlsConfig, Certificate, Identity, TlsServerStream};
+use log::{error, warn, info};
 use fdlimit::raise_fd_limit;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use serde_json::Value;
+use socks5::{parse_handshake_body, parse_request_body};
 
 fn reuse_socket(s: &TcpSocket) -> Result<(), Box<dyn Error>> {
     s.set_reuseaddr(true)?;
     Ok(())
+}
+
+async fn process_peer_stream(mut tls_peer_stream: TlsServerStream<TcpStream>, config: ServerConfig,
+                             tls_config: ServerTlsConfig) {
+    let mut buffer = [0_u8; 1024];
+
+    // read handshake data
+    let r = tls_peer_stream.read(&mut buffer).await;
+    if r.is_err() {
+        error!("tls peer stream read err: {}", r.unwrap_err());
+        return;
+    }
+    let v = (0..r.unwrap()).map(|x: usize| buffer[x]).collect();
+    let r = parse_handshake_body(&v);
+    if r.is_err() {
+        error!("parse handshake body err: {}", r.unwrap_err());
+        return;
+    }
+
+    let r = tls_peer_stream.write(&mut [0x05_u8, 0x00_u8]).await;
+    if r.is_err() {
+        error!("tls peer stream write err: {}", r.unwrap_err());
+        return;
+    }
+
+    // read request data
+    let r = tls_peer_stream.read(&mut buffer).await;
+    if r.is_err() {
+        error!("tls peer stream read err: {}", r.unwrap_err());
+        return;
+    }
+    let v = (0..r.unwrap()).map(|x: usize| buffer[x]).collect();
+    let r = parse_request_body(&v);
+    if r.is_err() {
+        error!("parse request body err: {}", r.unwrap_err());
+        return;
+    }
+    let (cmd, atype, host, port) = r.unwrap();
+
+    if cmd != Value::from(0x01_u8) {
+        warn!("cmd 0x{:x} not support", cmd);
+        return;
+    }
+    if atype != Value::from(0x04_u8) {
+        warn!("ip v6 not support");
+        return;
+    }
+    info!("proxy to: {}:{}", host.clone(), port);
+
+    let addr_conn: SocketAddr =
+        format!("{}:{}", host.clone(), port)
+            .parse()
+            .unwrap_or_else(|e| { panic!("parse proxy to addr err: {}", e) });
+
+    // new socket and set reuse
+    let socket_connect = TcpSocket::new_v4().unwrap();
+    reuse_socket(&socket_connect)
+        .unwrap_or_else(|e| { panic!("set connect socket reuse option err: {}", e) });
+
+    let connect_timeout = tokio::time::Duration::from_secs(5);
+    let r = tokio::time::timeout(
+        connect_timeout,
+        socket_connect.connect(addr_conn),
+    ).await;
+    if r.is_err() {
+        error!("tcp stream connect err: {}", r.unwrap_err());
+        return;
+    }
+    let r = r.unwrap();
+    if r.is_err() {
+        error!("tcp stream connect err: {}", r.unwrap_err());
+        return;
+    }
+    let tcp_proxy_stream = r.unwrap();
+
+    info!("client: tcp proxy conn established");
+
+    let r = tls_peer_stream.write(
+        &mut [0x05_u8, 0x00_u8, 0x00_u8, 0x01_u8, 0x00_u8, 0x00_u8, 0x00_u8, 0x00_u8, 0x00_u8, 0x00_u8])
+        .await;
+    if r.is_err() {
+        error!("tls peer stream write err: {}", r.unwrap_err());
+        return;
+    }
+
+    let (mut proxy_stream_reader, mut proxy_stream_writer) = tcp_proxy_stream.into_split();
+    let (mut tls_stream_reader, mut tls_stream_writer) = tokio::io::split(tls_peer_stream);
+
+    let fut1 = tokio::spawn(async move {
+        let r = tokio::io::copy(&mut tls_stream_reader, &mut proxy_stream_writer).await;
+        if r.is_err() {
+            error!("copy [tls->proxy] err: {}", r.unwrap_err());
+        }
+    });
+
+    let fut2 = tokio::spawn(async move {
+        let r = tokio::io::copy(&mut proxy_stream_reader, &mut tls_stream_writer).await;
+        if r.is_err() {
+            error!("copy [proxy->tls] err: {}", r.unwrap_err());
+        }
+    });
+
+    let (_, _) = tokio::join!(fut1, fut2);
 }
 
 #[tokio::main]
@@ -75,5 +181,11 @@ async fn main() {
         }
         let tls_peer_stream = r.unwrap();
         info!("accept tls connection of peer from {}", peer_addr);
+
+        let config_clone = config.clone();
+        let tls_config_clone = tls_config.clone();
+        tokio::spawn(async move {
+            process_peer_stream(tls_peer_stream, config_clone, tls_config_clone).await;
+        });
     }
 }
