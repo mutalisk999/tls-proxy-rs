5e6141e1ba8da3b1e99fd37bf977277d7df8adee
mutalisk999 <tangjian999999999@gmail.com>
2022-11-14 16:03:21 +0800

    update for tls-proxy-client

--- tls-proxy-client/src/main.rs
+++ tls-proxy-client/src/main.rs
@@ -1,11 +1,12 @@
-extern crate tokio;
-
 mod config;
 
 use std::net::SocketAddr;
-use tokio::net::TcpSocket;
+use tokio;
+use tokio::net::{TcpSocket, TcpStream};
 use std::error::Error;
 use config::{load_entire_file_content, ClientConfig};
+use tokio_tls_helper::{ClientTlsConfig, Certificate, Identity};
+use http::Uri;
 use log::{error, info};
 
 use crate::config::CONFIG_FILE_NAME;
@@ -15,10 +16,56 @@ fn reuse_socket(s: &TcpSocket) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+async fn process_peer_stream(peer_stream: TcpStream, config: &ClientConfig,
+                             tls_config: &ClientTlsConfig) {
+    let tls_connector = tls_config
+        .tls_connector(Uri::from_static("localhost"))
+        .unwrap_or_else(|e| { panic!("tls_connector build err: {}", e) });
+
+    let addr_conn: SocketAddr =
+        format!("{}:{}", config.server_host, config.server_port)
+            .parse()
+            .unwrap_or_else(|e| { panic!("parse server addr err: {}", e) });
+
+    let tcp_stream = TcpStream::connect(addr_conn)
+        .await.unwrap_or_else(|e| { panic!("tcp stream connect err: {}", e) });
+
+    let tls_stream = tls_connector.connect(tcp_stream)
+        .await.unwrap_or_else(|e| { panic!("tls stream connect err: {}", e) });
+
+    info!("client: tls conn established");
+
+    let (mut peer_stream_reader, mut peer_stream_writer) = peer_stream.into_split();
+    let (mut tls_stream_reader, mut tls_stream_writer) = tokio::io::split(tls_stream);
+
+    let fut1 = tokio::spawn(async move {
+        tokio::io::copy(&mut peer_stream_reader, &mut tls_stream_writer).await
+            .unwrap_or_else(|e| { panic!("copy [peer->tls] err: {}", e) });
+    });
+
+    let fut2 = tokio::spawn(async move {
+        tokio::io::copy(&mut tls_stream_reader, &mut peer_stream_writer).await
+            .unwrap_or_else(|e| { panic!("copy [tls->peer] err: {}", e) });
+    });
+
+    let (_, _) = tokio::join!(fut1, fut2);
+}
+
 #[tokio::main]
 async fn main() {
     let json_str = load_entire_file_content(CONFIG_FILE_NAME);
     let config = ClientConfig::from_json_str(&json_str);
+
+    let ca_cert_str = load_entire_file_content(config.ca_cert.as_str());
+    let ca_cert = Certificate::from_pem(ca_cert_str.as_bytes());
+
+    let key_str = load_entire_file_content(config.client_key.as_str());
+    let cert_str = load_entire_file_content(config.client_cert.as_str());
+    let client_identity = Identity::from_pem(cert_str.as_str(), key_str.as_str());
+
+    let tls_config = ClientTlsConfig::new()
+        .ca_certificate(ca_cert)
+        .identity(client_identity);
 
     let addr_listen: SocketAddr =
         format!("{}:{}", config.listen_host, config.listen_port)
@@ -45,9 +92,9 @@ async fn main() {
             error!("listener accept err: {}", r.unwrap_err());
             continue;
         }
-        let (mut tcp_stream, peer_addr) = r.unwrap();
+        let (peer_stream, peer_addr) = r.unwrap();
         info!("connection of peer from {}", peer_addr);
 
-        tokio::spawn(async move {});
+        process_peer_stream(peer_stream, &config, &tls_config).await;
     }
 }
